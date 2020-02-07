3bfa2625d7acb72abc89f5187e8e28a29833b143
jonandernovella <novella.uu@gmail.com>
2020-02-07 14:29:12 +0100

    Add basic config settings
    Logic for basic config settings has been implemented

--- backend/route.py
+++ backend/route.py
@@ -10,13 +10,15 @@ import logging
 
 logging.basicConfig(level=settings.LOG_LEVEL)
 
-app = Flask(__name__, static_folder='../frontend/')
+
+
+app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
 auth_blueprint = Blueprint('auth', __name__, url_prefix='/auth')
 
 
 @app.route('/')
 def ega_home():
-    return redirect(url_for('auth.authenticate_to_elixir'))    
+    return redirect(url_for('auth.authenticate_to_elixir'))
 
 
 @auth_blueprint.route('/')
@@ -30,10 +32,10 @@ def start_app(flask_app):
 
 def main():
     start_app(app)
-    logging.debug('>>>>> Starting Elixir server at http://{}:{} <<<<<'.format(settings.HOST,settings.PORT))
+    logging.debug(f'>>>>> Starting Elixir server at http://{settings.BIND_ADDRESS}:{settings.PORT} <<<<<')
     # Create gevent WSGI server
-    wsgi_server = WSGIServer((settings.HOST, settings.PORT),
-                             app)
+    wsgi_server = WSGIServer((settings.BIND_ADDRESS, settings.PORT),
+                             app.wsgi_app)
                             # certfile=settings.CERT_FILE,
                             # keyfile=settings.KEY_FILE,
                             # ca_certs=settings.CA_CERTS)
--- backend/settings.py
+++ backend/settings.py
@@ -1,12 +1,51 @@
-# Flask settings
-HOST = '0.0.0.0'
-PORT = 8080
-FLASK_DEBUG = False
+import os
+import sys
+import yaml
+import logging
 
-# This should be CRITICAL, ERROR, WARNING, INFO, DEBUG or NOTSET
-LOG_LEVEL = 'DEBUG'
 
-# Gevent settings
-CERT_FILE = None
-KEY_FILE = None
-CA_CERTS = None
+# YAML file settings
+
+ARG = "--settings_file"
+SETTINGS_FILE = "settings-sample.yaml"
+
+if ARG in sys.argv:
+    try:
+        SETTINGS_FILE = sys.argv[sys.argv.index(ARG)+1]
+    except IndexError:
+        logging.error("No argument for --settings_file")
+        sys.exit(1)
+
+try:
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    yaml_settings_fh = open(os.path.join(current_dir, SETTINGS_FILE))
+except FileNotFoundError:
+    parent_dir = os.path.join(current_dir, os.pardir)
+    yaml_settings_fh = open(os.path.join(parent_dir, SETTINGS_FILE))
+
+yaml_settings = yaml.safe_load(yaml_settings_fh)
+yaml_settings_fh.close()
+
+LOG_LEVEL =  yaml_settings["log_level"]
+ELIXIR_REDIRECT_URI = yaml_settings["elixir"]['redirectUri']
+ELIXIR_ID = yaml_settings["elixir"]['id']
+ELIXIR_SECRET = yaml_settings["elixir"]['secret']
+BIND_ADDRESS = yaml_settings["bindAddress"]
+PORT = yaml_settings["port"]
+
+# ENV settings
+
+if "LOG_LEVEL" in os.environ:
+    LOG_LEVEL = os.environ.get('LOG_LEVEL')
+
+if "ELIXIR_REDIRECT_URI" in os.environ:
+    ELIXIR_REDIRECT_URI = os.environ.get('ELIXIR_REDIRECT_URI')
+
+if "ELIXIR_SECRET" in os.environ:
+    ELIXIR_SECRET = os.environ.get('ELIXIR_SECRET')
+
+if "BIND_ADDRESS" in os.environ:
+    BIND_ADDRESS = os.environ.get('BIND_ADDRESS')
+
+if "PORT" in os.environ:
+    PORT = os.environ.get('PORT')
