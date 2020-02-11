df972bc2647d7363162ed9801fd6e8b8ea31a464
jonandernovella <novella.uu@gmail.com>
2020-02-11 13:26:55 +0100

    Add oidc auth logic
    Logic for interacting with oidc compliant servers has been inplemented, as well as a mock server for testing purposes

--- backend/auth.py
+++ backend/auth.py
@@ -1,4 +1,28 @@
-_OAUTH_AUTHORIZE_URL = "https://login.elixir-czech.org/oidc/authorize"
-_OAUTH_ACCESS_TOKEN_URL = "https://login.elixir-czech.org/oidc/token"
-_OAUTH_USERINFO_ENDPOINT = "https://login.elixir-czech.org/oidc/userinfo"
-_OAUTH_SETTINGS_KEY = 'elixir_oauth'
+from flask_pyoidc.provider_configuration import ProviderConfiguration, ProviderMetadata, ClientMetadata
+from settings import SERVICE_SETTINGS as config
+
+_CLIENT_ID = config['ELIXIR_ID']
+_CLIENT_SECRET = config['ELIXIR_SECRET']
+
+if config["DEVELOPMENT"] == True:
+    _AUTHORIZE_URL = "http://localhost.localdomain:9090/auth"
+    _ACCESS_TOKEN_URL = "http://localhost.localdomain:9090/token"
+    _USERINFO_ENDPOINT = "http://localhost.localdomain:9090/me"
+    _ISSUER_URL =  "http:/localhost.localdomain:9090"
+else:
+    _AUTHORIZE_URL = "https://login.elixir-czech.org/oidc/authorize"
+    _ACCESS_TOKEN_URL = "https://login.elixir-czech.org/oidc/token"
+    _USERINFO_ENDPOINT = "https://login.elixir-czech.org/oidc/userinfo"
+    _ISSUER_URL =  "https://login.elixir-czech.org/oidc"
+
+
+_CLIENT_METADATA = ClientMetadata(client_id=_CLIENT_ID, client_secret=_CLIENT_SECRET)
+
+_PROVIDER_METADATA = ProviderMetadata(issuer=_ISSUER_URL,
+                                     authorization_endpoint=_AUTHORIZE_URL,
+                                     userinfo_endpoint=_USERINFO_ENDPOINT,
+                                     jwks_uri=_ACCESS_TOKEN_URL)
+
+PROVIDER_CONFIG = ProviderConfiguration(provider_metadata=_PROVIDER_METADATA,
+                                        client_metadata=_CLIENT_METADATA,
+                                        auth_request_params={'response_type': 'code'})
--- backend/route.py
+++ backend/route.py
@@ -2,32 +2,59 @@ import os
 import requests
 import json
 import logging
+import datetime
 import auth
 from gevent.pywsgi import WSGIServer
+import flask
 from flask import (
     Blueprint, Flask, request, jsonify, redirect, url_for
 )
+from flask_pyoidc import OIDCAuthentication
+from flask_pyoidc.provider_configuration import ProviderConfiguration
+from flask_pyoidc.user_session import UserSession
 from settings import SERVICE_SETTINGS as config
 
 logging.basicConfig(level=config["LOG_LEVEL"])
 
 app = Flask(__name__, template_folder="../frontend/templates", static_folder="../frontend/static")
 
+app.config.update({'SERVER_NAME': config['SERVER_NAME'],
+                   #"OIDC_REDIRECT_ENDPOINT": config["ELIXIR_REDIRECT_URI"],
+                   'SECRET_KEY': config['SECRET_KEY'],
+                   'PERMANENT_SESSION_LIFETIME': datetime.timedelta(days=1).total_seconds(),
+                   'PREFERRED_URL_SCHEME': 'http',
+                   'DEBUG': True})
+
+# Setup OIDC Authenticator
+auth = OIDCAuthentication({'default': auth.PROVIDER_CONFIG}, app)
+
+# Start the Elixir Blueprint
 elixir_blueprint = Blueprint("auth", __name__, url_prefix="/elixir")
 
-@app.route("/", methods=['GET'])
+
+@app.route("/")
 def auth_home():
     return app.send_static_file("index.html")
 
 
-@elixir_blueprint.route("/login", methods=['GET'])
+@app.route("/redirect_uri")
+def redirection():
+    return "You have been redirected here"
+
+
+@elixir_blueprint.route("/login")
+@auth.oidc_auth('default')
 def login_to_elixir():
-    return redirect(auth._OAUTH_AUTHORIZE_URL, code=302)
+    user_session = UserSession(flask.session)
+    return jsonify(access_token=user_session.access_token,
+                   id_token=user_session.id_token,
+                   userinfo=user_session.userinfo)
 
 
-@elixir_blueprint.route("/logout", methods=['GET'])
+@elixir_blueprint.route("/logout")
+@auth.oidc_logout
 def logout_from_elixir():
-    return "Logging out..."
+    return "You have been successfully logged out from Elixir AAI"
 
 
 def start_app(flask_app):
@@ -39,12 +66,13 @@ def main():
     logging.debug(">>>>> Starting Elixir server at http://{}:{} <<<<<".format(config["BIND_ADDRESS"], config["PORT"]))
     # Create gevent WSGI server
     wsgi_server = WSGIServer((config["BIND_ADDRESS"], config["PORT"]),
-                             app.wsgi_app)
+                             app)
                             # certfile=config["CERT_FILE"],
                             # keyfile=config["KEY_FILE"],
                             # ca_certs=config["CA_CERTS"])
     # Start gevent WSGI server
     wsgi_server.serve_forever()
+    #app.run()
 
 
 if __name__ == "__main__":
