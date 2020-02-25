f53faeac2fd306973089212969eae8aba063661e
Jon Ander Novella <jonandernovella@users.noreply.github.com>
2020-02-25 16:36:09 +0100

    Change env override strategy
    With this strategy we achieve the desired hierarchy for variable handling.

--- backend/settings.py
+++ backend/settings.py
@@ -26,25 +26,16 @@ except FileNotFoundError:
 yaml_settings = yaml.safe_load(yaml_settings_fh)
 yaml_settings_fh.close()
 
-SERVICE_SETTINGS = { "LOG_LEVEL" :  yaml_settings.get("logLevel", "DEBUG"),
-                     "ELIXIR_REDIRECT_URI" : yaml_settings.get("elixir", {}).get("redirectUri", "/elixir/login"),
-                     "ELIXIR_ID" : yaml_settings.get("elixir", {}).get("id", "XC56EL11xx"),
-                     "ELIXIR_SECRET" : yaml_settings.get("elixir", {}).get("secret", "wHPVQaYXmdDHg"),
-                     "BIND_ADDRESS" : yaml_settings.get("bindAddress", "localhost"),
-                     "PORT" : yaml_settings.get("port", 31111),
-                     "SERVER_NAME" : yaml_settings.get("serverName", "localhost:31111"),
-                     "URL_SCHEME" : yaml_settings.get("urlScheme", "http"),
-                     "DEVELOPMENT" : yaml_settings.get("development", True),
-                     "SECRET_KEY" : yaml_settings.get("secretKey", "de8b3fe55c7d9fb32de24b8428470876f00021f88c9eb7ff"),
-                     "CERT_FILE" : yaml_settings.get("certFile", ""),
-                     "KEY_FILE" : yaml_settings.get("keyFile", ""),
-                     "CA_CERTS" :  yaml_settings.get("caCerts", "") }
-
-# ENV settings
-
-def overwrite_with_env(env_var):
-    if env_var in os.environ:
-        SERVICE_SETTINGS[env_var] = os.environ.get(env_var)
-
-for var in SERVICE_SETTINGS.keys():
-    overwrite_with_env(var)
+SERVICE_SETTINGS = { "LOG_LEVEL" : os.environ.get("LOG_LEVEL", yaml_settings.get("logLevel", "DEBUG")),
+                     "ELIXIR_REDIRECT_URI" : os.environ.get("ELIXIR_REDIRECT_URI", yaml_settings.get("elixir", {}).get("redirectUri", "/elixir/login")),
+                     "ELIXIR_ID" : os.environ.get("ELIXIR_ID", yaml_settings.get("elixir", {}).get("id", "XC56EL11xx")),
+                     "ELIXIR_SECRET" : os.environ.get("ELIXIR_SECRET", yaml_settings.get("elixir", {}).get("secret", "wHPVQaYXmdDHg")),
+                     "BIND_ADDRESS" : os.environ.get("BIND_ADDRESS", yaml_settings.get("bindAddress", "localhost")),
+                     "PORT" : int(os.environ.get("PORT", yaml_settings.get("port", 31111))),
+                     "SERVER_NAME" : os.environ.get("SERVER_NAME", yaml_settings.get("serverName", "localhost:31111")),
+                     "URL_SCHEME" : os.environ.get("URL_SCHEME", yaml_settings.get("urlScheme", "http")),
+                     "DEVELOPMENT" : eval(os.environ.get("DEVELOPMENT", yaml_settings.get("development", True))),
+                     "SECRET_KEY" : os.environ.get("SECRET_KEY", yaml_settings.get("secretKey", "de8b3fe55c7d9fb32de24b8428470876f00021f88c9eb7ff")),
+                     "CERT_FILE" : os.environ.get("CERT_FILE", yaml_settings.get("certFile", "")),
+                     "KEY_FILE" : os.environ.get("KEY_FILE", yaml_settings.get("keyFile", "")),
+                     "CA_CERTS" : os.environ.get("CA_CERTS", yaml_settings.get("caCerts", "")) }
