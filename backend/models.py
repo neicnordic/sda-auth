ed917be6d6f6f06d278ecbdf05964441c5604dbf
Johan Viklund <johan.viklund@nbis.se>
2020-02-26 14:35:29 +0100

    Proper call to super() in models.py

--- backend/models.py
+++ backend/models.py
@@ -1,13 +1,11 @@
 import flask_login
-from flask_login import UserMixin
 
 class EgaUser(flask_login.UserMixin):
     def __init__(self, ega_id, ega_secret=None):
         self.ega_id = ega_id
         self.ega_secret = ega_secret
-        UserMixin.__init__(self)
+        super()
     def get_id(self):
-           return (self.ega_id)
+        return (self.ega_id)
     def get_secret(self):
-           return (self.ega_secret)
-    pass
+        return (self.ega_secret)
