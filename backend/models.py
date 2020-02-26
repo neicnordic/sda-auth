d8ada991bfd8653b5b3cea11b33334e6aee8bc81
jonandernovella <novella.uu@gmail.com>
2020-02-26 20:20:00 +0100

    Refactor ega user class
    This class should contain the ega id and password of the user. It has
    been refactored to reflect better the content of the latter.

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,11 @@
 import flask_login
 
 class EgaUser(flask_login.UserMixin):
-    def __init__(self, ega_id, ega_secret=None):
+    def __init__(self, ega_id, ega_password=None):
         self.ega_id = ega_id
-        self.ega_secret = ega_secret
+        self.ega_password = ega_password
         super()
     def get_id(self):
         return self.ega_id
-    def get_secret(self):
-        return self.ega_secret
+    def get_password(self):
+        return self.ega_password
