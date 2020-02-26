0222aacea8bfc0e323c667d4056c3e686f7d3630
jonandernovella <novella.uu@gmail.com>
2020-02-26 20:12:00 +0100

    Refactor EGA authentication to do hash verification
    Password hash verification is done using the bcrypt python library. It
    is done once the CEGA id is verified and the user has a valid string as
    input.

--- backend/ega_authenticator.py
+++ backend/ega_authenticator.py
@@ -6,22 +6,37 @@ import flask_login
 from models import EgaUser
 import json
 from flask import render_template, jsonify, redirect, url_for
+import bcrypt
 
 
 _AUTHORISATION_URL = config['CEGA_AUTH_URL']
+_CEGA_ID = config['CEGA_ID']
+_CEGA_SECRET = config['CEGA_SECRET']
 
 
 def authenticate_with_ega(username, password):
-    ega_user = EgaUser(ega_id=username, ega_secret=password)
+    ega_user = EgaUser(ega_id=username,
+                       ega_password=password)
+
     id_type_payload = {"idType": "username"}
     response = requests.get(f'{_AUTHORISATION_URL}{ega_user.get_id()}',
                                 params=id_type_payload,
-                                auth=HTTPBasicAuth(ega_user.get_id(),
-                                                   ega_user.get_secret()))
+                                auth=HTTPBasicAuth(_CEGA_ID,
+                                                   _CEGA_SECRET))
+
     if response.status_code == 200:
-        flask_login.login_user(ega_user)
-        logging.info("You have been successfully logged in with EGA")
-        return response.json()
+        password_hash = json.loads(response.text)['response']['result'][0]['passwordHash']
+        password_is_correct = verify_password(response_password=password_hash,
+                                              user_password=ega_user.get_password())
+
+        if password_is_correct:
+            flask_login.login_user(ega_user)
+            logging.info("You have been successfully logged in with EGA")
+            return True
+        else:
+            logging.warning("Password hashes did not match")
+            return None
+
     else:
         logging.warning(f'{response.status_code}: {response.content}')
         return None
@@ -38,3 +53,7 @@ def is_logged_in():
         return flask_login.current_user
     else:
         return None
+
+
+def verify_password(response_password, user_password):
+    return bcrypt.checkpw(user_password.encode("utf-8"), response_password.encode("utf-8"))
