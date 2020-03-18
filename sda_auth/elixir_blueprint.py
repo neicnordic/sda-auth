3dd490b4738c2cd8c403b9c9abe1efd8704f349a
jonandernovella <novella.uu@gmail.com>
2020-03-18 17:30:28 +0100

    Reorganise elixir endpoints
    The callback endpoint has been removed and all requests are now handled
    in the /login endpoint.

--- sda_auth/elixir_blueprint.py
+++ sda_auth/elixir_blueprint.py
@@ -1,5 +1,7 @@
 import elixir_authenticator
-from flask import Blueprint, render_template, redirect, url_for
+import flask
+import logging
+from flask import Blueprint, render_template, redirect, url_for, jsonify
 
 
 elixir_bp = Blueprint("elixir", __name__, url_prefix="/elixir")
@@ -7,38 +9,38 @@ elixir_bp = Blueprint("elixir", __name__, url_prefix="/elixir")
 
 def login():
     """Sign in with Elixir."""
-    response = elixir_authenticator.authenticate_with_elixir()
-    if response is None:
-        return redirect(url_for("elixir.info"), 302)
-    else:
-        return elixir_authenticator.authenticate_with_elixir()
-
-
-def info():
-    """Display Elixir user info."""
-    user_session = elixir_authenticator.handle_uninitialised_session()
-    if user_session and user_session.is_authenticated():
-        print(user_session.userinfo)
+    req = flask.request.args
+    state = flask.session.get("state", None)
+    logging.debug("Request coming")
+    logging.debug(req)
+    if 'error' in req:
+        logging.debug("Response contains error")
+        return jsonify({"status": "Error"}), 401
+    elif 'code' in req and state is not None:
+        logging.debug("Code resp")
+        elixir_authenticator.handle_authentication_response()
+        tok_req = elixir_authenticator.send_token_request()
+        logging.debug("Access token resp")
+        token_resp = elixir_authenticator.handle_token_response(tok_req)
+        userinfo_req = elixir_authenticator.send_userinfo_request(token_resp)
+        logging.debug("User info token resp")
+        userinfo = elixir_authenticator.handle_userinfo_response(userinfo_req)
+        logging.debug(userinfo)
         return render_template("elixir_login_success.html",
-                               user_name=user_session.userinfo['sub'],
-                               access_token=user_session.access_token)
+                               user_name=userinfo['sub'],
+                               access_token=token_resp['access_token'],
+                               passport=userinfo.get('ga4gh_passport_v1', None))
     else:
-        return redirect(url_for("index"), 302)
+        logging.debug("Authenticating request...")
+        return elixir_authenticator.authenticate()
 
 
 def logout():
     """Sign out from Elixir."""
-    elixir_authenticator.logout_from_elixir()
     elixir_authenticator.revoke_token()
+    elixir_authenticator.logout_from_elixir()
     return redirect(url_for("index"), 302)
 
 
-def callback_uri():
-    """Register callback endpoint for login."""
-    return elixir_authenticator.handle_auth_response()
-
-
-elixir_bp.add_url_rule('/auth', 'login', view_func=login)
-elixir_bp.add_url_rule('/info', 'info', view_func=info)
 elixir_bp.add_url_rule('/logout', 'logout', view_func=logout)
-elixir_bp.add_url_rule('/login', 'callback_uri', view_func=callback_uri, methods=['GET', 'POST'])
+elixir_bp.add_url_rule('/login', 'login', view_func=login, methods=['GET', 'POST'])
