1888cb0a767a0a23590040554b2541d03cf0ea57
jonandernovella <novella.uu@gmail.com>
2020-03-18 17:29:39 +0100

    Refactor Elixir authenticator
    The OIDC authentication is now done by using the oic lib directly.

--- sda_auth/elixir_authenticator.py
+++ sda_auth/elixir_authenticator.py
@@ -1,13 +1,16 @@
-from flask_pyoidc.provider_configuration import ProviderConfiguration, ProviderMetadata, ClientMetadata
 from settings import SERVICE_SETTINGS as config
 import requests
 import flask
-from flask_pyoidc.user_session import UserSession, UninitialisedSession
 from requests.auth import HTTPBasicAuth
 import logging
-from route import app
-from flask_pyoidc import OIDCAuthentication
-
+from oic import rndstr
+from oic.oic import Client, RegistrationResponse, AuthorizationResponse, AuthorizationErrorResponse
+from oic.oic.message import ProviderConfigurationResponse
+from oic.oic import Grant
+from oic.oic import Token
+from oic.utils import time_util
+from oic.utils.authn.client import CLIENT_AUTHN_METHOD
+from werkzeug.utils import redirect
 
 _CLIENT_ID = config['ELIXIR_ID']
 _CLIENT_SECRET = config['ELIXIR_SECRET']
@@ -17,55 +20,141 @@ _JWKS_URL = config['ELIXIR_CERTS_URL']
 _USERINFO_URL = config['ELIXIR_USERINFO_URL']
 _ISSUER_URL = config['ELIXIR_ISSUER_URL']
 _TOKEN_REVOCATION_URL = config['ELIXIR_REVOCATION_URL']
-
-_CLIENT_METADATA = ClientMetadata(client_id=_CLIENT_ID, client_secret=_CLIENT_SECRET)
-
-_PROVIDER_METADATA = ProviderMetadata(issuer=_ISSUER_URL,
-                                      authorization_endpoint=_AUTHORISATION_URL,
-                                      userinfo_endpoint=_USERINFO_URL,
-                                      token_endpoint=_ACCESS_TOKEN_URL,
-                                      jwks_uri=_JWKS_URL)
-
-PROVIDER_CONFIG = ProviderConfiguration(provider_metadata=_PROVIDER_METADATA,
-                                        client_metadata=_CLIENT_METADATA)
-
-oidc_authenticator = OIDCAuthentication({'default': PROVIDER_CONFIG}, app)
+_ELIXIR_REDIRECT_URL = config['ELIXIR_REDIRECT_URI']
+_ELIXIR_SCOPE = config['ELIXIR_SCOPE']
 
 
-def authenticate_with_elixir():
-    """Authenticate with Elixir."""
-    session = UserSession(flask.session, "default")
-    pyoidc_fcd = oidc_authenticator.clients[session.current_provider]
-    if session.should_refresh(pyoidc_fcd.session_refresh_interval_seconds):
-        logging.debug('Refreshing user auth"')
-        return oidc_authenticator._authenticate(client=pyoidc_fcd, interactive=False)
-    elif session.is_authenticated():
-        logging.debug('User already authenticated')
-        return None
+client = Client(client_authn_method=CLIENT_AUTHN_METHOD)
+
+client_info = {"client_id": _CLIENT_ID,
+               "client_secret": _CLIENT_SECRET}
+
+client_reg = RegistrationResponse(**client_info)
+client.store_registration_info(client_reg)
+
+provider_info = {"issuer": _ISSUER_URL,
+                 "authorization_endpoint": _AUTHORISATION_URL,
+                 "token_endpoint": _ACCESS_TOKEN_URL,
+                 "userinfo_endpoint": _USERINFO_URL,
+                 "jwks_uri": _JWKS_URL}
+
+op_info = ProviderConfigurationResponse(**provider_info)
+client.handle_provider_config(op_info, op_info['issuer'])
+
+
+def authenticate(interactive=True):
+    """Authenticate user."""
+    flask.session['destination'] = flask.request.url
+    flask.session['state'] = rndstr()
+    flask.session['nonce'] = rndstr()
+
+    extra_auth_params = {}
+    if not interactive:
+        extra_auth_params['prompt'] = 'none'
+
+    login_url = send_authentication_request(flask.session['state'],
+                                            flask.session['nonce'],
+                                            extra_auth_params)
+    return redirect(login_url)
+
+
+def send_authentication_request(state, nonce, extra_auth_params):
+    """Send user auth request."""
+    args = {'response_type': 'code',
+            'grant_type': 'authorization_code',
+            'scope': _ELIXIR_SCOPE.split(),
+            'redirect_uri': _ELIXIR_REDIRECT_URL,
+            'state': state,
+            'nonce': nonce}
+
+    logging.debug(f'{_ELIXIR_REDIRECT_URL} is the redir')
+    args.update(extra_auth_params)
+    auth_request = client.construct_AuthorizationRequest(request_args=args,
+                                                         authn_method="client_secret_basic")
+
+    logging.debug('Sending authentication request: %s', auth_request.to_json())
+    return auth_request.request(_AUTHORISATION_URL)
+
+
+def handle_authentication_response():
+    """Handle auth response to get user info."""
+    auth_resp = flask.request.args
+    logging.debug('received authentication response')
+    authn_resp = parse_authentication_response(auth_resp)
+    logging.debug('handling authentication response: %s', authn_resp.to_json())
+    flask.session['code'] = authn_resp['code']
+
+
+def parse_authentication_response(response_params):
+    """Handle auth response."""
+    if 'error' in response_params:
+        response = AuthorizationErrorResponse(**response_params)
     else:
-        logging.debug('User not authenticated')
-        return oidc_authenticator._authenticate(client=pyoidc_fcd)
+        response = AuthorizationResponse(**response_params)
+        logging.debug("Verifying key jar")
+        response.verify(keyjar=client.keyjar)
+
+    assert flask.session['state'] == response['state']
+    return response
+
+
+def send_token_request():
+    """Send token request."""
+    args = {"response_type": 'access_token',
+            "grant_type": 'authorization_code',
+            "code": flask.session['code'],
+            "scope": _ELIXIR_SCOPE.split()}
+
+    grant = Grant()
+    grant.code = flask.session['code']
+    grant.grant_expiration_time = time_util.utc_time_sans_frac() + 30
+    client.grant = {flask.session['state']: grant}
+    logging.debug(flask.session.get("code"))
+    logging.debug('making token request: %s', args)
+    return client.do_access_token_request(state=flask.session["state"],
+                                          request_args=args,
+                                          authn_method="client_secret_basic")
+
+
+def handle_token_response(resp):
+    """Handle token response."""
+    logging.debug('handling token response: %s', resp.to_json())
+    #token_response = client.parse_response(AccessTokenResponse, info=json.dumps(resp.to_dict()))
+    flask.session['access_token'] = resp['access_token']
+    return resp
+
+
+def send_userinfo_request(token_resp):
+    """Send user info request."""
+    grant = Grant()
+    token = Token(token_resp)
+    grant.tokens.append(token)
+    client.grant[flask.session['state']] = grant
+    grant.tokens.append(token)
+    userinfo = client.do_user_info_request(state=flask.session["state"])
+    return userinfo
+
+
+def handle_userinfo_response(resp):
+    """Handle user info response."""
+    logging.debug('handling userinfo response: %s', resp.to_json())
+    #userinfo_response = client.parse_response(AccessTokenResponse, info=json.dumps(resp.to_dict()))
+    return resp
 
 
 def logout_from_elixir():
     """Sign out from Elixir."""
-    if 'state' in flask.request.args:
-        # returning redirect from provider
-        if flask.request.args['state'] != flask.session.pop('end_session_state'):
-            logging.error("Got unexpected state '%s' after logout redirect.", flask.request.args['state'])
-            return None
-
-    return oidc_authenticator._logout()
+    flask.session.pop("access_token")
 
 
 def revoke_token():
     """Revoke Elixir auth token."""
     logging.debug('Revoking token...')
-    user_session = handle_uninitialised_session()
+    user_session = flask.session
     if user_session is None:
         return None
     else:
-        token_payload = {"token": user_session.access_token}
+        token_payload = {"token": user_session['access_token']}
         response = requests.get(_TOKEN_REVOCATION_URL,
                                 params=token_payload,
                                 auth=HTTPBasicAuth(config["ELIXIR_ID"],
@@ -76,18 +165,3 @@ def revoke_token():
         else:
             logging.warning(f'{response.status_code}: {response.content}')
             return None
-
-
-def handle_uninitialised_session():
-    """Handle sessions that might not have been initialised."""
-    try:
-        session = UserSession(flask.session)
-        return session
-    except UninitialisedSession:
-        logging.debug('The user was already logged out')
-        return None
-
-
-def handle_auth_response():
-    """Handle auth response to get user info."""
-    return oidc_authenticator._handle_authentication_response()
