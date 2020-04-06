84ab4417771dd016dfb03c4f0900e85d175a08c0
jonandernovella <novella.uu@gmail.com>
2020-04-06 18:04:14 +0200

    Improve TLS config handling
    TLS config is now more granular and it allows CA-signed certs,
    self-signed and absence of certs.

--- sda_auth/route.py
+++ sda_auth/route.py
@@ -2,6 +2,7 @@ from gevent import monkey
 monkey.patch_all() # noqa
 
 import logging
+from pathlib import Path
 import datetime
 import sda_auth.elixir_blueprint as elixir_blueprint
 import sda_auth.ega_blueprint as ega_blueprint
@@ -49,13 +50,42 @@ def start_app(flask_app):
     flask_app.register_blueprint(ega_blueprint.ega_bp)
 
 
+def verify_exist(files):
+    """Check if the given files exist."""
+    for f in files:
+        if Path(f).is_file():
+            continue
+        else:
+            logging.error(f'{f} does not exist')
+            exit(1)
+
+
 def main():
     """Start the wsgi serving the application."""
     start_app(app)
     logging.debug(">>>>> Starting authentication server at {}:{} <<<<<".format(config["BIND_ADDRESS"], config["PORT"]))
+    logging.debug(f'TLS flag is {config["ENABLE_TLS"]}')
+
     # Create gevent WSGI server
-    wsgi_server = WSGIServer((config["BIND_ADDRESS"], config["PORT"]),
-                             app.wsgi_app)
+    if config["ENABLE_TLS"] and config["CA_CERTS"] is not None:
+        verify_exist([config["CERT_FILE"], config["KEY_FILE"], config["CA_CERTS"]])
+        wsgi_server = WSGIServer((config["BIND_ADDRESS"], config["PORT"]),
+                                 app.wsgi_app,
+                                 certfile=config["CERT_FILE"],
+                                 keyfile=config["KEY_FILE"],
+                                 ca_certs=config["CA_CERTS"])
+
+    elif config["ENABLE_TLS"] and config["CA_CERTS"] is None:
+        verify_exist([config["CERT_FILE"], config["KEY_FILE"]])
+        wsgi_server = WSGIServer((config["BIND_ADDRESS"], config["PORT"]),
+                                 app.wsgi_app,
+                                 certfile=config["CERT_FILE"],
+                                 keyfile=config["KEY_FILE"])
+
+    else:
+        wsgi_server = WSGIServer((config["BIND_ADDRESS"], config["PORT"]),
+                                 app.wsgi_app)
+
     # Start gevent WSGI server
     wsgi_server.serve_forever()
 
