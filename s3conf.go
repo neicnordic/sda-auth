70dc87a923134514c6a6f847a5de70d01a6a4806
jonandernovella <novella.uu@gmail.com>
2020-10-29 10:10:03 +0100

    Add s3conf script

---
+++ s3conf.go
@@ -0,0 +1,22 @@
+package main
+
+// Configure an OpenID Connect aware OAuth2 client.
+func getS3ConfigMap(token, inboxHost, user string) map[string]string {
+	s3conf := map[string]string{"access_key": user,
+		"secret_key":              user,
+		"access_token":            token,
+		"check_ssl_certificate":   "False",
+		"check_ssl_hostname":      "False",
+		"encoding":                "UTF-8",
+		"encrypt":                 "False",
+		"guess_mime_type":         "True",
+		"host_base":               inboxHost,
+		"host_bucket":             inboxHost,
+		"human_readable_sizes":    "True",
+		"multipart_chunk_size_mb": "50",
+		"use_https":               "True",
+		"socket_timeout":          "30",
+	}
+
+	return s3conf
+}
